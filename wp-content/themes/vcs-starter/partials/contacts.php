fd283e03148bdc7b48125fd626fd77ccb23ef719
EpicMod <e.bergner@gmail.com>
2018-11-29 13:08:36 +0200

    2018-11-29

---
+++ wp-content/themes/vcs-starter/partials/contacts.php
@@ -0,0 +1,20 @@
+		<section class="contact_page flex_container" id="id_contact_page">
+			<h2>CONTACT US</h2>
+			<div class="contact_form_container flex-container">
+				<div class="contact_form">
+					<div class="contact">
+					<?php 
+						echo do_shortcode(get_field('contact_form_shortcode'));					
+					?>
+					</div>
+				</div>
+				<div class="contact_details">
+					<p>Arvidas Bergner</p>
+					<p>Tel.: +370 620 73374</p>
+					<p>E-mail: arvidas.bergner@gmail.com</p>
+					<p class="link_paintings">
+						<a class="nostyle" href="paintings.html" target="_blank">www. PAVEIKSLAI .www</a>
+					</p>
+				</div>
+			</div>
+		</section>
